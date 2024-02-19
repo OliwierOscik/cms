5ea754e94fddd44ad23fe2359c35ea7585595a27
OliwierOscik <oscikoliwier@gmail.com>
2024-02-19 13:28:22 +0100

    new php

---
+++ index.php
@@ -0,0 +1,11 @@
+<body>
+    <header>
+        <h1>Nagłówek strony</h1>
+    </header>
+    <div id="container">
+        <?php
+        //nowe połączenie do bazy danych
+        $db = new mysqli('localhost' )
+
+    </div>
+</body>
